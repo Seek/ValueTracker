d53e5ec576189903c9216f280beb3c5938e4e1da
Joshua Tully <seekndestroyer@hotmail.com>
2016-05-20 17:21:30 -0500

    Getting SQL set in place

---
+++ update/hsdb.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+class HSDatabase():
+    
+    def __init__(self, path):
+        self._path = path
+        
+    def close(self):
+        pass
+        
+    def update_from_json():
+        pass
+    
+    
